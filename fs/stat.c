02e09c7561330cb163310482917578bca8eb3ecd
Seuj09 <Seuj09@users.noreply.github.com>
2026-09-13 07:29:22 +0000

    kernel: replace KernelSU-Next with ReSukiSU
    - Remove KernelSU-Next submodule (rifsxd fork)
    - Add ReSukiSU submodule (https://github.com/ReSukiSU/ReSukiSU, main)
    - Repoint drivers/kernelsu symlink -> ../ReSukiSU/kernel
    
    drivers/Makefile and drivers/Kconfig already reference kernelsu/
    generically from the prior integration, so no changes needed there.
    unisoc_defconfig already has CONFIG_KSU=y and CONFIG_KSU_MANUAL_HOOK=y
    set, which selects the correct entry in ReSukiSU's hook-method choice
    (default is KSU_TRACEPOINT_HOOK, GKI2 5.10+ only - not applicable to
    this 5.4 non-GKI kernel).

--- fs/stat.c
+++ fs/stat.c
@@ -419,6 +419,10 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 }
 #endif
 
+#ifdef CONFIG_KSU
+extern void ksu_handle_newfstat_ret(unsigned int *fd, struct stat __user **statbuf_ptr);
+#endif
+
 SYSCALL_DEFINE2(newfstat, unsigned int, fd, struct stat __user *, statbuf)
 {
 	struct kstat stat;
@@ -427,6 +431,9 @@ SYSCALL_DEFINE2(newfstat, unsigned int, fd, struct stat __user *, statbuf)
 	if (!error)
 		error = cp_new_stat(&stat, statbuf);
 
+#ifdef CONFIG_KSU
+	ksu_handle_newfstat_ret(&fd, &statbuf);
+#endif
 	return error;
 }
 #endif
@@ -546,6 +553,10 @@ SYSCALL_DEFINE2(lstat64, const char __user *, filename,
 	return error;
 }
 
+#ifdef CONFIG_KSU
+extern void ksu_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr); // optional
+#endif
+
 SYSCALL_DEFINE2(fstat64, unsigned long, fd, struct stat64 __user *, statbuf)
 {
 	struct kstat stat;
@@ -554,6 +565,9 @@ SYSCALL_DEFINE2(fstat64, unsigned long, fd, struct stat64 __user *, statbuf)
 	if (!error)
 		error = cp_new_stat64(&stat, statbuf);
 
+#ifdef CONFIG_KSU // for 32-bit
+	ksu_handle_fstat64_ret(&fd, &statbuf);
+#endif
 	return error;
 }
 
